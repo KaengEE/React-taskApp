40c6511e91e1603b7f50bd81938fc893471b0d2b
KaengEE <yopuiqwer@naver.com>
2023-11-21 09:37:51 +0900

    form 객체로관리

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import Tag from "./Tag";
 import "./TaskForm.css";
 
 export default function TaskForm() {
+  //각각의 스테이터스 관리
+  // const [task, setTask] = useState("");
+  // const [status, setStatus] = useState("todo");
+
+  //객체로 관리
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+  });
+  console.log(taskData.task, taskData.status);
+
+  //함수
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
   return (
     <header className="app_header">
       <form>
+        {/* 할일입력창 */}
         <input
+          name="task"
+          value={taskData.task}
+          onChange={handleChange}
           type="text"
           className="task_input"
           placeholder="할일을 입력해주세요."
@@ -20,10 +44,15 @@ export default function TaskForm() {
           </div>
           <div>
             {/* 옵션버튼 */}
-            <select className="task_status">
+            <select
+              name="status"
+              value={taskData.status}
+              onChange={handleChange}
+              className="task_status"
+            >
               <option value="todo">할일</option>
-              <option value="todo">진행중</option>
-              <option value="todo">완료</option>
+              <option value="doing">진행중</option>
+              <option value="done">완료</option>
             </select>
           </div>
           {/* 추가버튼 */}
